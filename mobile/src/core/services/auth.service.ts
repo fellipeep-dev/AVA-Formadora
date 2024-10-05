e716aa6d75d19c1556e4b258b1500c9f7df52cc9
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2024-10-04 23:38:51 -0300

    cleanUp

--- mobile/src/core/services/auth.service.ts
+++ mobile/src/core/services/auth.service.ts
@@ -1,27 +1,27 @@
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { AuthInterface } from "../shared/@types/auth";
-import { catchError, Observable, tap, throwError } from "rxjs";
-import { ApiServiceFactory } from "../common/factory/api.factory";
-import { Router } from "@angular/router";
-import { UserInterface } from "../shared/@types/user";
-
-
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthInterface } from '../shared/@types/auth';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { ApiServiceFactory } from '../common/factory/api.factory';
+import { Router } from '@angular/router';
+import { UserInterface } from '../shared/@types/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService extends ApiServiceFactory<AuthInterface> {
   constructor(http: HttpClient, private router: Router) {
-    super(http)
+    super(http);
   }
   login(data: Pick<UserInterface, 'email' | 'password'>) {
-    return this.http.post<UserInterface>(`${this.baseUrl}/user/auth`, data).pipe(
-      tap((response) => {
-        localStorage.setItem('user',JSON.stringify(response.id!));
-      }),
-      catchError(this.handleError)
-    );
+    return this.http
+      .post<UserInterface>(`${this.baseUrl}/user/auth`, data)
+      .pipe(
+        tap((response) => {
+          localStorage.setItem('user', JSON.stringify(response.id!));
+        }),
+        catchError(this.handleError)
+      );
   }
   create(data: UserInterface) {
     return this.http.post<UserInterface>(`${this.baseUrl}/user`, data).pipe(
@@ -48,13 +48,11 @@ export class AuthService extends ApiServiceFactory<AuthInterface> {
   }
 
   logout() {
-    localStorage.removeItem('user')
-    this.router.navigate(['/begin'])
+    localStorage.removeItem('user');
+    this.router.navigate(['/begin']);
   }
 
   isAuthenticated(): boolean {
     return !!localStorage.getItem('user');
   }
-
-  
 }
